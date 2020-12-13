3bac82e56df43d06ff8bb9230d10c8b42560c286
Luis Arismendi <luisfelipe.arismendi@gmail.com>
2020-12-12 21:15:30 -0500

    remove gitignore files

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    methodOverride = require("method-override"),
-    mongoose = require("mongoose");
+'use strict';
 
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override"),
+  mongoose = require("mongoose");
+
+// Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', (err, res) => {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+// Import Models and controllers
+var models     = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require("./controllers/tvshows");
+
+// Example Route
 var router = express.Router();
-
-router.get('/', function(req, res) {
-    res.send("Hello Word!");
+router.get("/", (req, res) => {
+  res.send("Hello Word!");
 });
-
 app.use(router);
 
-app.listen(3000, function () {
-    console.log("Node server running on http://localhost:3000")
+// API routes
+var tvshows = express.Router();
+
+tvshows
+  .route("/tvshows")
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows
+  .route("/tvshows/:id")
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use("/api", tvshows);
+
+app.listen(3000, () => {
+  console.log("Node server running on http://localhost:3000");
 });
